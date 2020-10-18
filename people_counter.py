f7f298515502b31e2952005f6bcb8c69e854b1ef
Josecisneros001 <joseacisnerosm@gmail.com>
2020-10-18 17:07:20 -0500

    remove visual frame view in python script

--- people_counter.py
+++ people_counter.py
@@ -261,7 +261,7 @@ while True:
 		writer.write(frame)
 
 	# show the output frame
-	cv2.imshow("Frame", frame)
+	# cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
 	# if the `q` key was pressed, break from the loop
