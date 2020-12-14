216188ff4221ae2858b2a2f334796e8e535e780e
Josecisneros001 <joseacisnerosm@gmail.com>
2020-12-14 13:02:10 -0600

    change skip_frames

--- people_counter_v3.py
+++ people_counter_v3.py
@@ -41,7 +41,7 @@ ap.add_argument("-o", "--output", type=str,
 	help="path to optional output video file")
 ap.add_argument("-c", "--confidence", type=float, default=0.22,
 	help="minimum probability to filter weak detections")
-ap.add_argument("-s", "--skip-frames", type=int, default=50,
+ap.add_argument("-s", "--skip-frames", type=int, default=35,
 	help="# of skip frames between detections")
 args = vars(ap.parse_args())
 
