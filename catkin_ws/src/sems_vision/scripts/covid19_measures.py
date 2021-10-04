9577efe95127c086b3fc705b1ebee673de73d258
Josecisneros001 <joseacisnerosm@gmail.com>
2021-10-04 16:16:12 -0500

    hotfix faces

--- catkin_ws/src/sems_vision/scripts/covid19_measures.py
+++ catkin_ws/src/sems_vision/scripts/covid19_measures.py
@@ -407,7 +407,7 @@ class CamaraProcessing:
 
                             # Preprocess Image
                             face = frame[startY:endY, startX:endX]
-                            if len(face) == 0 or startY == startX or endX == endY:
+                            if len(face) == 0 or startX >= endX or startY >= endY:
                                 continue
                             face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
                             face = cv2.resize(face, (224, 224))
