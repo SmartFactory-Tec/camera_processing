8e2a30bdd973d1a47361484a7fddf7b50d0553d3
Josecisneros001 <joseacisnerosm@gmail.com>
2021-10-04 16:45:32 -0500

    fix opencv dependency utils

--- catkin_ws/src/sems_vision/include/sems_vision_utils.py
+++ catkin_ws/src/sems_vision/include/sems_vision_utils.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+
+# import Dependencies
 import math
+import cv2
 
 def map(x, in_min, in_max, out_min, out_max):
     return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
