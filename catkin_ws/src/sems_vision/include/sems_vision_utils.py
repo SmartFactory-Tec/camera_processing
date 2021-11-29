da9b27f7b56971594af3ac2f8687c399e847e262
Josecisneros001 <joseacisnerosm@gmail.com>
2021-11-29 08:44:23 -0600

    hotfix FLT_EPSILON

--- catkin_ws/src/sems_vision/include/sems_vision_utils.py
+++ catkin_ws/src/sems_vision/include/sems_vision_utils.py
@@ -3,6 +3,8 @@
 import math
 import cv2
 
+FLT_EPSILON = sys.float_info.epsilon
+
 def map(x, in_min, in_max, out_min, out_max):
     return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
